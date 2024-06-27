dc4da0c4f57aea1018daee52b21b962aabd82cfb
Rameez <shaikhrameez516@gmail.com>
2024-06-27 14:22:13 +0530

    shirt.jsx file added

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
   development: {
-    backendUrl: "http://localhost:8080/api/v1/dalle",
+    backendUrl: "https://custom-3d-tshirt-1.onrender.com/api/v1/dalle",
   },
   production: {
     backendUrl: "https://devswag.onrender.com/api/v1/dalle",
